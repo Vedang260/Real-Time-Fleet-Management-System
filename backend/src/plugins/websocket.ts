1ac218d76f541837a1b63ba1e6f8526549768960
Vedang <vedang.wappnet@gmail.com>
2025-04-18 12:05:14 +0530

    websocket plugin is added

---
+++ backend/src/plugins/websocket.ts
@@ -0,0 +1,7 @@
+import fp from 'fastify-plugin';
+import { FastifyInstance } from 'fastify';
+import websocket from '@fastify/websocket';
+
+export default fp(async (fastify: FastifyInstance) => {
+  fastify.register(websocket);
+});
