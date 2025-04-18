bcd09ce9068ec67c2cbca30d12d7b717b652b662
Vedang <vedang.wappnet@gmail.com>
2025-04-18 11:27:36 +0530

    created the user entity

---
+++ backend/src/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Role } from '../enums/role.enums';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.MANAGER,
+  })
+  role: Role;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
