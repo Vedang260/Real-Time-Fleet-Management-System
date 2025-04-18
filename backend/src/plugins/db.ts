a5c012907ca5df3dc231df29b289308a1f7dc0e2
Vedang <vedang.wappnet@gmail.com>
2025-04-18 12:01:20 +0530

    db connection is added

---
+++ backend/src/plugins/db.ts
@@ -0,0 +1,15 @@
+import fp from 'fastify-plugin';
+import { FastifyInstance } from 'fastify';
+import { typeOrmConfig } from '../config/ormconfig';
+import { createConnection } from 'typeorm';
+
+export default fp(async (fastify: FastifyInstance) => {
+  try {
+    const connection = await createConnection(typeOrmConfig);
+    fastify.log.info('Database connected successfully');
+    fastify.decorate('db', connection);
+  } catch (err) {
+    fastify.log.error('Database connection error:', err);
+    throw new Error('Failed to connect to database');
+  }
+});
