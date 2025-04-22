1ed172f466ecd32eae548230bbf7acd6bac8bb0f
Vedang <vedang.wappnet@gmail.com>
2025-04-22 11:50:22 +0530

    created location interfaces

--- frontend/src/types/location.ts
+++ frontend/src/types/location.ts
@@ -1,4 +1,19 @@
 export interface Location{
+    vehicleId: string;
     latitude: number;
     longitude: number;
+}
+
+export interface LocationHistory{
+    locationId: string;
+    latitude: number;
+    longitude: number;
+    vehicleId: string;
+    recordedAt: string;
+}
+
+export interface LocationHistoryResponse{
+    success: boolean;
+    message: string;
+    locationHistory: LocationHistory[];
 }
