5330d5ed504f3e2de7ef363dbb759ef01bc92d5e
Vedang <vedang.wappnet@gmail.com>
2025-04-23 11:10:17 +0530

    generateRoutes is added

--- backend/src/utils/generateRoutes.utils.ts
+++ backend/src/utils/generateRoutes.utils.ts
@@ -1,26 +1,33 @@
-import { google } from 'googleapis';
+import axios from 'axios';
 import * as polyline from '@mapbox/polyline';
+import dotenv from 'dotenv';
+import { LocationPoint } from '../dtos/routes.dto';
 
-export async function generateRoute(startLocation: { lat: number; lng: number }, destinationLocation: { lat: number; lng: number }) {
-  const maps = google.maps({ version: '3.45', auth: process.env.GOOGLE_API_KEY });
+dotenv.config();
 
-  const directionsResponse = await maps.directions({
-    params: {
-      origin: `${startLocation.lat},${startLocation.lng}`,
-      destination: `${destinationLocation.lat},${destinationLocation.lng}`,
-      mode: 'driving',
-      key: process.env.GOOGLE_API_KEY,
-    },
-  });
+export async function generateRoute(startLocation: LocationPoint, destinationLocation: LocationPoint) {
+    try{
+        const apiKey = process.env.GOOGLE_API_KEY;
+        const origin = `${startLocation.latitude},${startLocation.longitude}`;
+        const destination = `${destinationLocation.latitude},${destinationLocation.longitude}`;
 
-  const route = directionsResponse.data.routes[0];
-  if (!route) throw new Error('No route found from Google Maps API');
+        const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=driving&key=${apiKey}`;
 
-  const encodedPolyline = route.overview_polyline.points;
-  const decodedCoords = polyline.toGeoJSON(encodedPolyline).coordinates.map(([lng, lat]) => ({
-    lat,
-    lng,
-  }));
+        const response = await axios.get(url);
+        const route = response.data.routes[0];
+        console.log("Generated routes: ", route);
+        if (!route || !route.overview_polyline) {
+            throw new Error('No route found from Google Maps API');
+        }
 
-  return decodedCoords;
+        const encodedPolyline = route.overview_polyline.points;
+        const decodedCoords = polyline
+            .toGeoJSON(encodedPolyline)
+            .coordinates.map(([longitude, latitude]) => ({ latitude, longitude }));
+
+        return decodedCoords;
+    }catch(error: any){
+        console.log('Error in generation routes: ', error.message);
+        throw new Error('Failed to generate the routes');
+    }
 }
